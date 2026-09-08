c151a4c28f11e48f45e47fa123e24441d0362157
Jesús Acosta <jgacosta.24@est.ucab.edu.ve>
2026-09-08 17:28:01 -0400

    Versión de sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.app.gastosimple"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.app.gastosimple"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
